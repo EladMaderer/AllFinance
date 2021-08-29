4a7fff464149ad456f5509953178f8cc7e9ce391
Elad Maderer <eladm@isracard.co.il>
2021-08-29 11:32:15 +0300

    First commit - app init

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{backgroundColor: 'red', flex: 1}}>
+      <View>
+        <Text>Clean sheet</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+// const styles = StyleSheet.create({
+//
+// });
+
+export default App;
