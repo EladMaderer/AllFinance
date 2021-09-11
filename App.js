45edfeea392e65afb44e821013eb871b692569bb
Elad Maderer <eladm@isracard.co.il>
2021-09-11 15:41:43 +0300

    Added redux persist

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {Provider} from 'react-redux';
-import store from './src/store';
+import {store, persistor} from './src/store';
+import {PersistGate} from 'redux-persist/integration/react';
 import Navigation from './src/navigation/Navigation';
 
 //rsi - creating a React functional component
 
-const App = () => (
-  <Provider store={store}>
-    <Navigation />
-  </Provider>
-);
+const App = () => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Navigation />
+      </PersistGate>
+    </Provider>
+  );
+};
 
 export default App;
--- src/store.js
+++ src/store.js
@@ -1,11 +1,18 @@
 import {createStore, combineReducers} from 'redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {persistStore, persistReducer} from 'redux-persist';
 import signInReducer from './reducers/signInReducer';
 
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['userSignedIn'],
+};
+
 const rootReducer = combineReducers({
-  signInReducer,
+  signInReducer: persistReducer(persistConfig, signInReducer),
 });
 
 const configureStore = () => createStore(rootReducer);
-const store = configureStore();
-
-export default store;
+export const store = configureStore();
+export const persistor = persistStore(store);
